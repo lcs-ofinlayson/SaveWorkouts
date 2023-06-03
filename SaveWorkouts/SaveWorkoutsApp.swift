dd7534f44e9a46d2b7ecf4547f2b24ed40e38981
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-06-03 10:43:42 -0400

    Initial Commit

---
+++ SaveWorkouts/SaveWorkoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveWorkoutsApp.swift
+//  SaveWorkouts
+//
+//  Created by Oliver Finlayson on 2023-06-03.
+//
+
+import SwiftUI
+
+@main
+struct SaveWorkoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
