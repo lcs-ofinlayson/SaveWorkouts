39b16b722dadae9b02c8e94756785b28886f7f38
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-06-05 11:59:04 -0400

    Made a structure for workout name and exercise name, along with making text fields for each, and added a navigation title.

--- SaveWorkouts/WorkoutView.swift
+++ SaveWorkouts/WorkoutView.swift
@@ -5,4 +5,39 @@
 //  Created by Oliver Finlayson on 2023-06-05.
 //
 
-import Foundation
+import SwiftUI
+
+struct WorkoutView: View {
+    @State var workoutName: String = ""
+    @State var exerciseName: String = ""
+  
+    var body: some View {
+        VStack {
+            TextField("Workout Name", text: $workoutName)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            TextField("Exercise Name", text: $exerciseName)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            WorkoutView()
+                .navigationTitle("Workout Tracker")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
